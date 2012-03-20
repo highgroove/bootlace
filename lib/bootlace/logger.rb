f2a9493b56c5bba814dad5b2e0fbca0a28f60712
Will Farrington <wcfarrington@gmail.com>
2012-03-19 23:29:12 -0400

    Logger fixes. 0.1.3

--- lib/bootlace/logger.rb
+++ lib/bootlace/logger.rb
@@ -4,20 +4,14 @@ module Bootlace
   module Logger
     attr_reader :logger
 
-    def initialize
-      if ENV["TEST"]
-        @logger = ::Logger.new('/tmp/bootlace.log')
-      else
-        @logger = ::Logger.new(STDOUT)
-      end
-      set_logger_format
+    if ENV["TEST"]
+      @logger = ::Logger.new('/tmp/bootlace.log')
+    else
+      @logger = ::Logger.new(STDOUT)
     end
 
-    private
-    def set_logger_format
-      logger.formatter = proc do |severity, datetime, progname, msg|
-        "[#{progname}] #{severity}: #{msg}\n"
-      end
+    @logger.formatter = proc do |severity, datetime, progname, msg|
+      "[#{progname}] #{severity}: #{msg}\n"
     end
   end
 end
